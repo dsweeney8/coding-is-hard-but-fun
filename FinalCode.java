d32fc0aa3008db64d747629a502d014abda84fc9
dsweeney8 <57074920+dsweeney8@users.noreply.github.com>
2019-10-27 17:02:40 -0400

    Add files via upload

--- FinalCode.java
+++ FinalCode.java
@@ -1,9 +1,73 @@
+import java.util.Scanner;
+
 
 public class FinalCode {
 
+	private static final int brown = 0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.print("Enter your first name");
+	System.out.print("Enter your last name");
+	System.out.print("Enter your age");
+	System.out.print("Enter your birthday month");}
 
+	 int month = 1-12;
+	   String monthString;
+	    
+	   {			
+	   System.out.print("Enter your favorite ROYGBIV color");
+	   System.out.print("Type in Help");
+	   // The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.
+	 
+	   System.out.print("Enter number of siblings");}
+	   {
+		 Scanner input   = new Scanner(System.in);
+		 
+		 System.out.println("What month were you born in");
+		 int monthBorn = input.nextInt();
+		 // if user was born on a odd month they have brown eyes
+		 int oddMonth = 1;
+		 if ("monthBornOdd"); 
+		 
+		 oddMonth = brown;
+		 int evenMonth = 2;}
+	   {
+		
+		   System.out.print("Enter your number of dogs");  
+		   int city = 3;
+		   
+		if (dog >= 0); 
+		city = cleveland; {
+		
+		if (dogs == 1);
+		city = maimi; {
+		
+			if (dogs == 2);
+			city = dayton;
+			// Depending on how many dogs you have, shows what city you live in
+		}
+		
+				
+			
+		
+			
+			
+			
+		}
+		
+				
+		
+		
+		 
+		 
+			 
+		
+			 
+		 }
+		 
+		 
+		 
+	   
+			   
+	
 	}
-
-}
+	 
